041738f6184154ae9ed66fc410402e1e7f00b9aa
Sayan Saha <sayan199783@gmail.com>
2025-01-11 23:32:15 +0530

    Add destination controller component

--- src/components/AddDestination.jsx
+++ src/components/AddDestination.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function AddDestination() {
+    const [newCity,setNewCity] = useState("");
+    const [newCountry,setNewCountry] = useState("")
   return (
     <div className='p-4 border'>
         <form>
             <div className='row col-8 offset-2'>
                 <h4>Enter a new destination</h4>
                 <div className='col-5 p-1'>
-                    <input type='text' className='form-control' placeholder='Enter City....' />
+                    <input type='text' className='form-control' placeholder='Enter City....' 
+                    value={newCity} onClick={(e) => setNewCity(e.target.value)}/>
                 </div>
                 <div className='col-5 p-1'>
-                <input type='text' className='form-control' placeholder='Enter Country....' />
+                <input type='text' className='form-control' placeholder='Enter Country....' value={newCountry} onClick={(e) => setNewCity(e.target.value)}/>
                 </div>
                 <div className='col-2 p-1'>
                     <button className='btn btn-success form-control'>Add</button>
