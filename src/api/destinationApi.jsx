0fc8d91601d87da2fab75ff843c3ff7ef3c80b92
Sayan Saha <sayan199783@gmail.com>
2025-01-12 11:22:14 +0530

    POST

--- src/api/destinationApi.jsx
+++ src/api/destinationApi.jsx
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const destinationAPI = createApi({
   reducerPath: "apidestination",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3000/" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5001/" }),
   endpoints: (builder) => ({
     //QUERY -> GET
     //MUTATION -> POST/PUT/DELETE
--- src/components/AddDestination.jsx
+++ src/components/AddDestination.jsx
@@ -14,18 +14,21 @@ function AddDestination() {
              country : newCountry, 
              daysNeeded : parseInt(Math.random() *10) + 1 
         })
+        setNewCity("");
+        setNewCountry("");
     }
   return (
     <div className='p-4 border'>
-        <form >
+        <form onSubmit={handleSubmit} >
             <div className='row col-8 offset-2'>
                 <h4>Enter a new destination</h4>
                 <div className='col-5 p-1'>
                     <input type='text' className='form-control' placeholder='Enter City....' 
-                    />
+                    value={newCity} onChange={(e) => setNewCity(e.target.value)}/>
                 </div>
                 <div className='col-5 p-1'>
-                <input type='text' className='form-control' placeholder='Enter Country....' />
+                <input type='text' className='form-control' placeholder='Enter Country....'
+                value={newCountry} onChange={(e) => setNewCountry(e.target.value)} />
                 </div>
                 <div className='col-2 p-1'>
                     <button className='btn btn-success form-control'>Add</button>
