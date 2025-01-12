8ef2e934642725577232beda8b885940196d66ac
Sayan Saha <sayan199783@gmail.com>
2025-01-12 11:42:51 +0530

    TAGS in RTK

--- src/api/destinationApi.jsx
+++ src/api/destinationApi.jsx
@@ -2,32 +2,37 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const destinationAPI = createApi({
   reducerPath: "apidestination",
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5001/" }),
+  tagTypes : ["Detinations"],
   endpoints: (builder) => ({
     //QUERY -> GET
     //MUTATION -> POST/PUT/DELETE
     getAllDestination: builder.query({
       query: () => "destination",
+      providesTags : ["Destinations"]
     }),
     addDestination: builder.mutation({
       query: (destination) => ({
         url : "destination",
         method : "POST",
         body : destination
-      }) 
+      }) ,
+      invalidatesTags : ["Destinations"]
     }),
     updateDestination: builder.mutation({
       query: (destination) => ({
         url : `destination/${destination.id}`,
         method : "PUT",
         body : destination
-      }) 
+      }) ,
+      invalidatesTags : ["Destinations"]
     }),
     deleteDestination: builder.mutation({
       query: ({id}) => ({
         url : `destination/${id}`,
         method : "DELETE",
         body : id
-      }) 
+      }) ,
+      invalidatesTags : ["Destinations"]
     })
   }),
 });
